6badb5fac0f104d94d842f5e96c0f86dd926a90f
David Runemalm <david.runemalm@gmail.com>
2022-12-27 16:19:06 +0100

    Add domain service base class.

---
+++ src/DDD/Domain/Services/DomainService.cs
@@ -0,0 +1,27 @@
+﻿using DDD.Application.Settings;
+using DDD.Domain.Model.Auth;
+using DDD.Domain.Services.Auth;
+using DDD.Logging;
+
+namespace DDD.Domain.Services
+{
+	public class DomainService : IDomainService
+	{
+		protected readonly ICredentials _credentials;
+		protected readonly ISettings _settings;
+		protected readonly ILogger _logger;
+		protected readonly IAuthDomainService _authDomainService;
+
+		public DomainService(
+			ICredentials credentials,
+			ISettings settings,
+			ILogger logger,
+			IAuthDomainService authDomainService)
+		{
+			_credentials = credentials;
+			_settings = settings;
+			_logger = logger;
+			_authDomainService = authDomainService;
+		}
+	}
+}
