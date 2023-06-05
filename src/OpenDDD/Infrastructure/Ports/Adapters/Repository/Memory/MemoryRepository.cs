4cee33c6072dbfed89fd9a84a16c02302703cf76
David Runemalm <david.runemalm@gmail.com>
2023-06-05 17:20:22 +0200

    Serialize & deserialize data in memory repository.

--- src/OpenDDD/Infrastructure/Ports/Adapters/Repository/Memory/MemoryRepository.cs
+++ src/OpenDDD/Infrastructure/Ports/Adapters/Repository/Memory/MemoryRepository.cs
@@ -6,17 +6,27 @@ using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Namotion.Reflection;
+using Newtonsoft.Json;
 using OpenDDD.Application;
 using OpenDDD.Domain.Model.BuildingBlocks.Aggregate;
 using OpenDDD.Domain.Model.BuildingBlocks.Entity;
+using OpenDDD.Infrastructure.Ports.Adapters.Common.Translation.Converters;
 using OpenDDD.Infrastructure.Ports.Repository;
 
 namespace OpenDDD.Infrastructure.Ports.Adapters.Repository.Memory
 {
 	public abstract class MemoryRepository<T> : Repository<T>, IRepository<T>, IStartableRepository where T : IAggregate
 	{
-		protected BlockingCollection<T> Items = new BlockingCollection<T>();
-		
+		protected ConcurrentDictionary<string, string> Data = new ConcurrentDictionary<string, string>();
+		private IMigrator<T> _migrator;
+		private readonly ConversionSettings _conversionSettings;
+
+		public MemoryRepository(IMigrator<T> migrator, ConversionSettings conversionSettings)
+		{
+			_migrator = migrator;
+			_conversionSettings = conversionSettings;
+		}
+
 		public override void Start(CancellationToken ct)
 		{
 			
@@ -38,7 +48,7 @@ namespace OpenDDD.Infrastructure.Ports.Adapters.Repository.Memory
 		}
 
 		public override void DeleteAll(ActionId actionId, CancellationToken ct)
-			=> Task.FromResult(Items = new BlockingCollection<T>());
+			=> Task.FromResult(Data = new ConcurrentDictionary<string, string>());
 
 		public override Task DeleteAllAsync(ActionId actionId, CancellationToken ct)
 		{
@@ -48,29 +58,31 @@ namespace OpenDDD.Infrastructure.Ports.Adapters.Repository.Memory
 
 		public override Task DeleteAsync(EntityId entityId, ActionId actionId, CancellationToken ct)
 		{
-			Items = new BlockingCollection<T>(
-				new ConcurrentQueue<T>(
-					Items.Where(i => i.Id != entityId)));
+			Data.Remove(entityId.Value, out string _);
 			return Task.CompletedTask;
 		}
 
 		public override IEnumerable<T> GetAll(ActionId actionId, CancellationToken ct)
-			=> Items;
+		{
+			var aggregates = Data.Select(kvp => JsonConvert.DeserializeObject<T>(kvp.Value, _conversionSettings));
+			aggregates = _migrator.Migrate(aggregates).ToList();
+			return aggregates;
+		}
 
 		public override async Task<IEnumerable<T>> GetAllAsync(ActionId actionId, CancellationToken ct)
 			=> await Task.FromResult(GetAll(actionId, ct));
 		
 		public override T Get(EntityId entityId, ActionId actionId, CancellationToken ct)
-			=> Items.FirstOrDefault(i => i.Id == entityId);
+			=> GetAll(actionId, ct).FirstOrDefault(i => i.Id == entityId);
 
 		public override async Task<T> GetAsync(EntityId entityId, ActionId actionId, CancellationToken ct)
 			=> await Task.FromResult(Get(entityId, actionId, ct));
 		
 		public override async Task<IEnumerable<T>> GetAsync(IEnumerable<EntityId> entityIds, ActionId actionId, CancellationToken ct)
-			=> await Task.FromResult(Items.Where(i => entityIds.Contains(i.Id)));
+			=> await Task.FromResult((await GetAllAsync(actionId, ct)).Where(i => entityIds.Contains(i.Id)));
 
 		public override T GetFirstOrDefaultWith(Expression<Func<T, bool>> where, ActionId actionId, CancellationToken ct)
-			=> Items.Where(where.Compile()).FirstOrDefault();
+			=> GetAll(actionId, ct).Where(where.Compile()).FirstOrDefault();
 
 		public override Task<T> GetFirstOrDefaultWithAsync(Expression<Func<T, bool>> where, ActionId actionId, CancellationToken ct)
 			=> Task.FromResult(GetFirstOrDefaultWith(where, actionId, ct));
@@ -82,7 +94,7 @@ namespace OpenDDD.Infrastructure.Ports.Adapters.Repository.Memory
 			=> (await GetWithAsync(andWhere, actionId, ct)).FirstOrDefault();
 
 		public override IEnumerable<T> GetWith(Expression<Func<T, bool>> where, ActionId actionId, CancellationToken ct)
-			=> Items.Where(where.Compile()).ToList().AsEnumerable();
+			=> GetAll(actionId, ct).Where(where.Compile()).ToList().AsEnumerable();
 
 		public override Task<IEnumerable<T>> GetWithAsync(Expression<Func<T, bool>> where, ActionId actionId, CancellationToken ct)
 			=> Task.FromResult(GetWith(where, actionId, ct));
@@ -91,7 +103,7 @@ namespace OpenDDD.Infrastructure.Ports.Adapters.Repository.Memory
 		{
 			var filtered = new List<T>();
 			
-			foreach (var item in Items)
+			foreach (var item in GetAll(actionId, ct))
 			{
 				foreach (var tuple in andWhere)
 				{
@@ -109,13 +121,9 @@ namespace OpenDDD.Infrastructure.Ports.Adapters.Repository.Memory
 
 		public override void Save(T aggregate, ActionId actionId, CancellationToken ct)
 		{
-			if (!Items.Contains(aggregate))
-				Items.Add(aggregate);
-			else
-				Items =
-					new BlockingCollection<T>(
-						new ConcurrentQueue<T>(
-							Items.Select(i => i.Id == aggregate.Id ? aggregate : i)));
+			var aggregateId = aggregate.Id.ToString();
+			var data = JsonConvert.SerializeObject(aggregate, _conversionSettings);
+			Data[aggregateId] = data;
 		}
 
 		public override Task SaveAsync(T aggregate, ActionId actionId, CancellationToken ct)
@@ -123,7 +131,7 @@ namespace OpenDDD.Infrastructure.Ports.Adapters.Repository.Memory
 			Save(aggregate, actionId, ct);
 			return Task.CompletedTask;
 		}
-
+		
 		public override string GetNextIdentity()
 			=> Guid.NewGuid().ToString();
 
