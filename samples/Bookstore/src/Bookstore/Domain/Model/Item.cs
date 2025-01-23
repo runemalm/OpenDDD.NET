e255f53162bb14b731f18c452ab67b66122ed019
David Runemalm <david.runemalm@gmail.com>
2025-01-23 15:03:04 +0100

    Add order aggregate and repository interface to sample project.

---
+++ samples/Bookstore/src/Bookstore/Domain/Model/Item.cs
@@ -0,0 +1,16 @@
+﻿using OpenDDD.Domain.Model;
+
+namespace Bookstore.Domain.Model
+{
+    public class Item : IValueObject
+    {
+        public string BookName;
+        public float Price;
+
+        public Item(string bookName, float price)
+        {
+            BookName = bookName;
+            Price = price;
+        }
+    }
+}
---
+++ samples/Bookstore/src/Bookstore/Domain/Model/Order.cs
@@ -0,0 +1,17 @@
+﻿using OpenDDD.Domain.Model.Base;
+
+namespace Bookstore.Domain.Model
+{
+    public class Order : AggregateRootBase<Guid>
+    {
+        public Guid CustomerId;
+        public IEnumerable<Item> Items;
+
+        public Order(Guid id, Guid customerId, IEnumerable<Item> items) : base(id)
+        {
+            CustomerId = customerId;
+            Items = items;
+        }
+    }
+}
+
