c34a73d25481a3bec07f2e5a5c87566e0b275606
David Runemalm <david.runemalm@gmail.com>
2025-02-15 17:24:39 +0100

    Update sample project.

--- samples/Bookstore/src/Bookstore/Infrastructure/Persistence/OpenDdd/Seeders/InMemory/InMemoryOpenDddBooksSeeder.cs
+++ samples/Bookstore/src/Bookstore/Infrastructure/Persistence/OpenDdd/Seeders/InMemory/InMemoryOpenDddBooksSeeder.cs
@@ -18,14 +18,14 @@ namespace Bookstore.Infrastructure.Persistence.OpenDdd.Seeders.InMemory
         {
             const string tableName = "books";
 
-            var existingBooks = await session.LoadAllAsync<string>(tableName, ct);
+            var existingBooks = await session.SelectAllAsync<string>(tableName, ct);
             if (existingBooks.Any()) return;
 
             var book1 = Book.Create("Domain-Driven Design: Tackling Complexity in the Heart of Software", "Eric Evans", 2003);
             var book2 = Book.Create("Implementing Domain-Driven Design", "Vaughn Vernon", 2013);
 
-            await session.SaveAsync(tableName, book1.Id, _serializer.Serialize<Book, Guid>(book1), ct);
-            await session.SaveAsync(tableName, book2.Id, _serializer.Serialize<Book, Guid>(book2), ct);
+            await session.UpsertAsync(tableName, book1.Id, _serializer.Serialize<Book, Guid>(book1), ct);
+            await session.UpsertAsync(tableName, book2.Id, _serializer.Serialize<Book, Guid>(book2), ct);
         }
     }
 }
