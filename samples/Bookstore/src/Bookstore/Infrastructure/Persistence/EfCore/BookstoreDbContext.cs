e3d8f105cf4ab17f58130b81de077564848dd5b0
David Runemalm <david.runemalm@gmail.com>
2025-02-16 12:59:48 +0100

    Add missing configuration to sample project ef core config.

--- samples/Bookstore/src/Bookstore/Infrastructure/Persistence/EfCore/BookstoreDbContext.cs
+++ samples/Bookstore/src/Bookstore/Infrastructure/Persistence/EfCore/BookstoreDbContext.cs
@@ -7,7 +7,9 @@ namespace Bookstore.Infrastructure.Persistence.EfCore
 {
     public class BookstoreDbContext : OpenDddDbContextBase
     {
+        public DbSet<Book> Books { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
 
         public BookstoreDbContext(DbContextOptions<BookstoreDbContext> options, OpenDddOptions openDddOptions, ILogger<BookstoreDbContext> logger)
             : base(options, openDddOptions, logger)
