a1f8af3e5313bab514aec9a93a1f40dbb1a14ceb
David Runemalm <david.runemalm@gmail.com>
2025-02-17 11:49:57 +0100

    Add tests for serializers.

---
+++ src/OpenDDD/Tests/Domain/Model/TestEntity.cs
@@ -0,0 +1,21 @@
+﻿using OpenDDD.Domain.Model.Base;
+
+namespace OpenDDD.Tests.Domain.Model
+{
+    public class TestEntity : EntityBase<Guid>
+    {
+        public string Description { get; private set; }
+
+        private TestEntity() { }
+
+        private TestEntity(Guid id, string description) : base(id)
+        {
+            Description = description;
+        }
+
+        public static TestEntity Create(string description)
+        {
+            return new TestEntity(Guid.NewGuid(), description);
+        }
+    }
+}
---
+++ src/OpenDDD/Tests/Domain/Model/TestValueObject.cs
@@ -0,0 +1,18 @@
+﻿using OpenDDD.Domain.Model;
+
+namespace OpenDDD.Tests.Domain.Model
+{
+    public class TestValueObject : IValueObject
+    {
+        public int Number { get; private set; }
+        public string Text { get; private set; }
+
+        private TestValueObject() { }
+
+        public TestValueObject(int number, string text)
+        {
+            Number = number;
+            Text = text;
+        }
+    }
+}
