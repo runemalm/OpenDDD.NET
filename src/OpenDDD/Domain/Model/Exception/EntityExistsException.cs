4301b6c1a1ab2984e43070c8425fad60223862e2
David Runemalm <david.runemalm@gmail.com>
2025-02-17 16:47:49 +0100

    Add domain exception classes.

---
+++ src/OpenDDD/Domain/Model/Exception/EntityExistsException.cs
@@ -0,0 +1,10 @@
+﻿using OpenDDD.Domain.Model.Exception.Base;
+
+namespace OpenDDD.Domain.Model.Exception
+{
+    public class EntityExistsException : DomainExceptionBase
+    {
+        public EntityExistsException(string entityName, string constraint)
+            : base($"{entityName} already exists with {constraint}.") { }
+    }
+}
