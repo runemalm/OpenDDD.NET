874bc5a844bd708b89669cf69967f697c72bb600
David Runemalm <david.runemalm@gmail.com>
2025-06-29 16:47:27 +0200

    Add constructor to DomainExceptionBase.

--- src/OpenDDD/Domain/Model/Exception/Base/DomainExceptionBase.cs
+++ src/OpenDDD/Domain/Model/Exception/Base/DomainExceptionBase.cs
@@ -3,5 +3,8 @@
     public abstract class DomainExceptionBase : System.Exception
     {
         protected DomainExceptionBase(string message) : base(message) { }
+        
+        protected DomainExceptionBase(string message, System.Exception innerException)
+            : base(message, innerException) { }
     }
 }
--- src/OpenDDD/Domain/Model/Exception/DomainException.cs
+++ src/OpenDDD/Domain/Model/Exception/DomainException.cs
@@ -4,7 +4,9 @@ namespace OpenDDD.Domain.Model.Exception
 {
     public class DomainException : DomainExceptionBase
     {
-        public DomainException(string message)
-            : base(message) { }
+        public DomainException(string message) : base(message) { }
+
+        public DomainException(string message, System.Exception innerException)
+            : base(message, innerException) { }
     }
 }
